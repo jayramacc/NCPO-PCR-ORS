b3c15f2d59db79496c7e89aaae5142af0c18947d
rolando ramos <jayr.amacc@gmail.com>
2018-04-04 18:10:59 +0800

    first commit

---
+++ assets/js/custom_home.js
@@ -0,0 +1,53 @@
+﻿
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+                      
+            //HOVER DROPDOWN SCRIPTS
+            $('ul.nav li.dropdown').hover(function () {
+                $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+            }, function () {
+                $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+            });
+
+            //TESTIMONIAL & SLIDEER SPEED
+            $('.carousel').carousel({
+                interval: 4000 //TIME IN MILLI SECONDS
+            })
+            // PRETTYPHOTO FUNCTION 
+
+            $("a.preview").prettyPhoto({
+                social_tools: false
+            });
+           
+            /*====================================
+            WRITE YOUR SCRIPTS HERE
+            ======================================*/
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
