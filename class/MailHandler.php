57f8b9e275a492ca7a836e8a9af58b7fb3674db8
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-05 08:23:12 +0800

    Prepared the Mailer file and included the repository

---
+++ class/MailHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+require '../vendor/autoload.php';
+
+$mail = new PHPMailer;
+
+$mail->isSMTP();
+$mail->SMTPDebug = 0;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->Username = "pcrncponaga@gmail.com";
+$mail->Password = "1234567890Jr";
+$mail->setFrom('pcrncponaga@gmail.com', 'Do not reply');
+$mail->addReplyTo('do_not_reply@gmail.com', 'Do not reply');
+$mail->addAddress($_GET["client_email"], $_GET["client_name"]);
+$mail->Subject = 'NCPO - Verify your Private key.';
+$mail->msgHTML(file_get_contents('../email_template.html'), __DIR__);
+$mail->send();
