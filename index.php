c0d140f7a5c9e98d74aee64d0c74b708c115ef47
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-07 04:54:02 +0800

    Renovate index.php

--- index.php
+++ index.php
@@ -1,174 +1,127 @@
 ﻿<!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!-->
 <html lang="en">
-<!--<![endif]-->
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-     <!--[if IE]>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <![endif]-->
     <title>NCPO Online Registrations Site</title>
     <!--  Bootstrap Style -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
      <!--  Font-Awesome Style -->
     <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
      <!--  Font-Awesome Animation Style -->
     <link href="assets/css/font-awesome-animation.css" rel="stylesheet" />
      <!--  Custom Style -->
     <link href="assets/css/style_home.css" rel="stylesheet" />
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
+    <style>
+        .carousel{
+            height: 200px;
+        }
+    </style>
 </head>
 <body>
-
-    <?php include 'includes/nav.php'; ?>
-
+    <?php include 'assets/includes/nav.php'; ?>
     <!--./ Top Menu End -->
     <div class="div-social-top">
-        <i class="fa fa-globe "></i>FB Page: NCPO Naga  | <i class="fa fa-mobile "></i>Call: :  (054) 473.3537 / (054) 478.8918.  |  <i class="fa fa-map-marker "></i>Country : PH &nbsp;
-        <a href="#"> <i class="fa fa-facebook-square "></i> </a>
-        <a href="#"> <i class="fa fa-linkedin-square "></i> </a>
-        <a href="#"> <i class="fa fa-pinterest-square "> </i> </a>
-    </div>
-    
-    <div class="middle-section">
         <div class="container">
-            <div class="row ">
-                <div class="col-lg-12 col-md-12 ">
-                    <h1>Steps to Apply Naga City Police Clearance</h1>
-                    <div id="testimonials" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#testimonials" data-slide-to="0" class="active"></li>
-                            <li data-target="#testimonials" data-slide-to="1" class=""></li>
-                            <li data-target="#testimonials" data-slide-to="2" class=""></li>
-                        </ol>
-                        <div class="carousel-inner">
-                            <div class="item active ">
-                                <div class="container center">
-                                    <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 slide-custom">
-                                        <h4>
-                                            <i class="fa fa-quote-left"></i>
-                                            Fill Out the form and Please take note of your reference code after registration (Estimate duration: 5 minutes) 
-                                            <i class="fa fa-quote-right"></i>
-                                        </h4>
-                                        <h5 class="pull-right"> <b>Online registration</b> </h5>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="item">
-                                <div class="container center">
-                                    <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 slide-custom">
-                                        <h4>
-                                            <i class="fa fa-quote-left"></i>
-                                            Go to Verification Panel to send us the reservation for the application 
-                                            <i class="fa fa-quote-right"></i>
-                                        </h4>
-                                        <h5 class="pull-right"> <b>Verification Process</b> </h5>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="item ">
-                                <div class="container center">
-                                    <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 slide-custom">
-                                        <h4>
-                                            <i class="fa fa-quote-left"></i>
-                                            Present the following requirements in the NCPO naga along with your reference number and payment.
-                                            <i class="fa fa-quote-right"></i>
-                                         </h4>
-                                        <h5 class="pull-right"> <b>Claiming Cleance in NCPO</b> </h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <i class="fa fa-globe "></i>
+            FB Page: NCPO Naga  | <i class="fa fa-mobile "></i>
+            Call: :  (054) 473.3537 / (054) 478.8918.  |  <i class="fa fa-map-marker "></i>
+            Country : PH &nbsp;
+            <a href="#"> <i class="fa fa-facebook-square "></i> </a>
+            <a href="#"> <i class="fa fa-linkedin-square "></i> </a>
+            <a href="#"> <i class="fa fa-pinterest-square "> </i> </a>
+        </div>
+        
+    </div>
+    <div class="middle-section">
+        <h1 style="color: #ff6a00" class="font-weight-bold mt-4">Steps to Apply Naga City Police Clearance</h1>
+        <div id="testimonials" class="carousel slide container" data-ride="carousel">
+            <div class="carousel-inner mt-lg-5" role="listbox">
+                <div class="carousel-item item active">
+                    <h4 class="w-75 container">
+                        <i class="fa fa-quote-left"></i>
+                        Fill Out the form and Please take note of your reference code after registration (Estimate duration: 5 minutes) 
+                        <i class="fa fa-quote-right"></i>
+                    </h4>
+                    <h5 class="pull-right font-weight-bold">Online Registration</h5>
+                </div>
+                <div class="carousel-item item">
+                    <h4 class="w-75 container">
+                        <i class="fa fa-quote-left"></i>
+                        Go to Verification Panel to send us the reservation for the application 
+                        <i class="fa fa-quote-right"></i>
+                    </h4>
+                    <h5 class="pull-right"> <b>Verification Process</b> </h5>
+                </div>
+                <div class="carousel-item item">
+                    <h4 class="w-75 container">
+                        <i class="fa fa-quote-left"></i>
+                        Present the following requirements in the NCPO naga along with your reference number and payment.
+                        <i class="fa fa-quote-right"></i>
+                    </h4>
+                    <h5 class="pull-right"> <b>Claiming Cleance in NCPO</b> </h5>
                 </div>
             </div>
+            <ol class="carousel-indicators">
+                <li data-target="#testimonials" data-slide-to="0" class="active"></li>
+                <li data-target="#testimonials" data-slide-to="1"></li>
+                <li data-target="#testimonials" data-slide-to="2"></li>
+            </ol>
         </div>
     </div>
-    <!--./ Testimonials End -->
-  
-    <section id="service-info">
-        <div class="container">
-            <div class="row text-center">
-                <div class="col-lg-12 col-md-12">
-                    <h3 style="color: blue"><strong> Police Commisions Relation in Naga City Police Office  Online Registration System  </strong></h3>
-                   
-                </div>
-            </div>
-            <div class="row pad-top-botm">
-                <div class="col-lg-4 col-md-4">
-                    <div class="media">
-                        <div class="pull-left">
+    <div id="service-info" class="pad-top-botm container">
+        <h3 class="font-weight-bold text-primary"> 
+            Police Commisions Relation in Naga City Police Office  Online Registration System.
+        </h3>
+        <div class="row pad-top-botm">
+            <div class="col-4">
+                <div class="card p-3">
+                    <div class="card-body">
+                        <div class="pull-left mr-2">
                             <i class="  fa fa-globe fa-4x rotate-icon  "></i>
                         </div>
-                        <div class="media-body">
-                            <h3 class="media-heading">Online <br> Registration</h3>
-                            <p>
-                                Fill out the registration form. Upon Completion of registering online. You will also recieve a notification via email of the reference number. please check your email.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4">
-                    <div class="media">
-                        <div class="pull-left">
-                            <i class="  fa fa-camera fa-4x rotate-icon "></i>
-                        </div>
-                        <div class="media-body">
-                            <h3 class="media-heading">Picture Upload</h3>
-                            <p>
-                                Taking photo on site. you can also upload your photo online and must wear shirts with collar
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4">
-                    <div class="media">
-                        <div class="pull-left">
-                            <i class="fa fa-print fa-4x rotate-icon "></i>
-                        </div>
-                        <div class="media-body">
-                            <h3 class="media-heading">Printing</h3>
-                            <p>Present the Requirements Needed and payment to recieve the police clearance form</p>
-                        </div>
+                        <h3 class="card-title font-weight-bold">Online <br> Registration</h3>
+                        <p class="card-text">
+                            Fill out the registration form. Upon Completion of registering online. You will also recieve a notification via email of the reference number. please check your email.
+                        </p>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <!--./ Service info Section End -->
-
-    <!--./ footer-sec  End -->
-    <div id="footser-end">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-md-12">
-                    2018 All Rights Reserved | by: <a href="https://github.com/jayramacc/NCPO-PCR-ORS">NCPO-PCR-ORS</a>
+            <div class="col-4">
+                <div class="card p-3">
+                    <div class="card-body">
+                        <div class="pull-left mr-2">
+                            <i class="  fa fa-camera fa-4x rotate-icon "></i>
+                        </div>
+                        <h3 class="card-title font-weight-bold">Picture Upload</h3>
+                        <p class="card-text">
+                            Taking photo on site. you can also upload your photo online and must wear shirts with collar
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card p-3">
+                    <div class="card-body">
+                        <div class="pull-left mr-2">
+                            <i class="fa fa-print fa-4x rotate-icon "></i>
+                        </div>
+                        <h3 class="card-title font-weight-bold">Printing</h3>
+                        <p class="card-text">Present the Requirements Needed and payment to recieve the police clearance form</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <!--./ footer-end End -->
-    <!--  Jquery Core Script -->
-    <script src="assets/js/jquery-1.10.2_home.js"></script>
-    <!--  Core Bootstrap Script -->
-    <script src="assets/js/bootstrap_home.js"></script>
-    <!--  Custom Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function () {
             // Initialize Carousel
             $('.carousel').carousel({
-                    interval: 4000 //TIME IN MILLI SECONDS
+                interval: 2000 //TIME IN MILLI SECONDS
             })
         });
     </script>
-
 </body>
 </html>
