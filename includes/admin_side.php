34cdba9bb9479bbb21ae99ed3e201b5c45a0eafa
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-07 02:31:48 +0800

    Fixed Sidebar options

--- includes/admin_side.php
+++ includes/admin_side.php
@@ -2,22 +2,23 @@
     <form class="quick_search">
         <input type="text" value="Reference # / Full Name" onfocus="if(!this._haschanged){this.value=''};this._haschanged=true;">
     </form>
+    
     <hr/>
+    
     <h3>ADMIN</h3>
     <ul class="toggle">
+        <li class="icn_new_article"><a href="admin.php">Dashboard</a></li>
         <li class="icn_new_article"><a href="onlineform.php">Notification</a></li>
-        <!--<li class="icn_tags"><a href="#">Tags</a></li>-->
     </ul>
-    <h3>APPLICANT</h3>
+
+    <h3>FEATURES</h3>    
     <ul class="toggle">
-        <li class="icn_add_user"><a href="add_new_user.php">Print Applicant Form</a></li>
         <li class="icn_folder"><a href="database.php">Applicant List</a></li>
+        <li class="icn_add_user"><a href="add_new_user.php">Print Applicant Form</a></li>
+        <li class="icn_photo"><a href="cloud.php">Online Backup</a></li>        
     </ul>
-    <h3>FEATURES</h3>
-    <ul class="toggle">
-        <li class="icn_photo"><a href="cloud.php">Online Backup</a></li>
-    </ul>
-    <h3>Admin</h3>
+    
+    <h3>OPTION</h3>
     <ul class="toggle">
         <li class="icn_security"><a href="changepass.php">Change Password</a></li>
         <li class="icn_jump_back"><a href="login.php">Logout</a></li>
