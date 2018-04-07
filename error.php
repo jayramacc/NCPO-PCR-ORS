b11b7a433038a3c3d85afbb3413cf0c747cb7e0a
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-07 11:24:19 +0800

    Fixed Include in each client file + renovate reuest-renew.php

--- error.php
+++ error.php
@@ -1,41 +1,19 @@
 ﻿<!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!-->
+
 <html lang="en">
-<!--<![endif]-->
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-     <!--[if IE]>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <![endif]-->
     <title>FAQ</title>
-    <!--  Bootstrap Style -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-     <!--  Font-Awesome Style -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
     <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
-     <!--  Font-Awesome Animation Style -->
     <link href="assets/css/font-awesome-animation.css" rel="stylesheet" />
-     <!--  Pretty Photo Style -->
-    <link href="assets/css/prettyPhoto.css" rel="stylesheet" />
-        <!--  Google Font Style -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-     <!--  Custom Style -->
     <link href="assets/css/style_home.css" rel="stylesheet" />
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-
-    <?php include 'includes/nav.php' ?>
+    <?php include 'assets/includes/nav.php' ?>
 
     <!--./ Top Menu End -->
   <div class="div-social-top">
--- login.php
+++ login.php
@@ -48,19 +48,16 @@ if (isset($un) && isset($pw)){
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Login</title>
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <title>Login</title>   
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
     <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
     <link href="assets/css/font-awesome-animation.css" rel="stylesheet" />
-    <link href="assets/css/prettyPhoto.css" rel="stylesheet" />
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
     <link href="assets/css/style_home.css" rel="stylesheet" />
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-
-    <?php include 'includes/nav.php'; ?>
+    <?php include 'assets/includes/nav.php' ?>
     
 <div class="clearfix"></div>
     <div class="container">
