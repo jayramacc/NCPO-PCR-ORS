d843f58fd7a2788f63df70acc1ee8eb6dd58314c
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-07 22:37:07 +0800

    Renovated cloud.php

--- admin/cloud.php
+++ admin/cloud.php
@@ -1,87 +1,42 @@
+<?php
+include_once "../class/AccountHandler.php";
 
-<!doctype html>
+// Check if the account is logged or not
+if(!AccountHandler::isLogin()){
+		header("location: ../login.php");
+    exit;
+}
+
+?>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="utf-8"/>
-    <title>Local Backup</title>
-    
-    <link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" />
-    <!--[if lt IE 9]>
-    <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" />
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-    <script src="js/jquery-1.5.2.min.js" type="text/javascript"></script>
-    <script src="js/hideshow.js" type="text/javascript"></script>
-    <script src="js/jquery.tablesorter.min.js" type="text/javascript"></script>
-    <script type="text/javascript" src="js/jquery.equalHeight.js"></script>
-    <script type="text/javascript">
-    $(document).ready(function() 
-        { 
-          $(".tablesorter").tablesorter(); 
-     } 
-    );
-    $(document).ready(function() {
-
-    //When page loads...
-    $(".tab_content").hide(); //Hide all content
-    $("ul.tabs li:first").addClass("active").show(); //Activate first tab
-    $(".tab_content:first").show(); //Show first tab content
-
-    //On Click Event
-    $("ul.tabs li").click(function() {
-
-        $("ul.tabs li").removeClass("active"); //Remove any "active" class
-        $(this).addClass("active"); //Add "active" class to selected tab
-        $(".tab_content").hide(); //Hide all tab content
-
-        var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
-        $(activeTab).fadeIn(); //Fade in the active ID content
-        return false;
-    });
-
-});
-    </script>
-    <script type="text/javascript">
-    $(function(){
-        $('.column').equalHeight();
-    });
-</script>
-
+	<meta charset="utf-8"/>
+	<title>Administrator Panel</title>
+	<link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="../assets/css/font-awesome.min.css" rel="stylesheet" />
+	<link href="../assets/css/font-awesome-animation.css" rel="stylesheet" />
+	<link href="../assets/css/admin.css" rel="stylesheet" />
+	<script src="../assets/js/jquery.min.js"></script>
+	<script src="../assets/js/bootstrap.bundle.min.js"></script>
 </head>
-
-
 <body>
-
-	<header id="header">
-		<hgroup>
-			<h1 class="site_title"><a href="admin.php">PCR Admin</a></h1>
-			<h2 class="section_title">Dashboard</h2><div class="btn_view_site"><a href="logout.php">Log out </a></div>
-		</hgroup>
-	</header> <!-- end of header bar -->
-	
-	<section id="secondary_bar">
-		<div class="user">
-			<p>ADMIN (<a href="admin.php"> HOME </a>)</p>
-			<!-- <a class="logout_user" href="#" title="Logout">Logout</a> -->
-		</div>
-		<div class="breadcrumbs_container">
-			<article class="breadcrumbs">
-				<a href="admin.php">Website Admin</a>
-				 	<div class="breadcrumb_divider"></div>
-				  	<a class="current">Dashboard</a>
-				</article>
-		</div>
-	</section><!-- end of secondary bar -->
-	
-	<?php include 'includes/admin_side.php'; ?>
-    <section id="main" class="column">
-		<h4 class="alert_info">Online Storage Backup on Local</h4>
-		<p style="margin-left: 40px">Backup the Latest data by clicking the button below.</p>
-		<button style="float:right; margin-right: 20px; font-weight: bold; padding: 9px; border-radius: 15px; background: #d4d4d4; border: 1px solid #847b7b; text-decoration: none; ">
-			<a href="class/Backup.php" >Backup Data</a>
-		</button>
-	</section>
+	<?php include '../assets/includes/adminnav.php'; ?>
+	<div class="page">
+		<?php include '../assets/includes/admin_inner.php'; ?>
+		<!-- Page Content will be here  -->
+        <div class="container card p-3 mt-3">
+            <div class="alert alert-info" role="alert">
+                <h4 class="alert-heading">Well done!</h4>
+                <p>This page will pull every information in the cloud server into your local computer, this is to secure every data even a server fallout happens.</p>
+                <hr>
+                <p class="mb-0">Whenever you need to, click "Backup Data" button below.</p>
+            </div>
+            <div>
+    			<a class="btn btn-primary pull-right" href="../class/Backup.php" >Backup Data</a>
+            </div>
+        </div>
+		<?php include '../assets/includes/admin_foot.php'; ?>
+	</div>
 </body>
-
 </html>
