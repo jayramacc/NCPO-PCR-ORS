b7c865a2d4db00264eeb9c499872c9c036e806b5
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-08 15:29:20 +0800

    Added Alert Message - Private Key Verification

--- class/verify.php
+++ class/verify.php
@@ -1,17 +1,26 @@
 <?php
-
 include 'MySqlLeaf.php';
+include 'FlashCard.php';
 
 $pk = $_GET["pk"];
 
-$sql = "UPDATE `applicants` SET `activated`='true' WHERE `private_key`='$pk';";
+$sql = "INSERT INTO `notification`(`private_key`, `type`) VALUES ('$pk', 'verify');";
 $query = mysqli_query(MySqlLeaf::getCon(), $sql);
 
 if ($query === true){
-    $sql = "INSERT INTO `notification`(`private_key`, `type`) VALUES ('$pk', 'verify');";
-    mysqli_query(MySqlLeaf::getCon(), $sql);
-    echo "Successfull registered";
+    $sql = "UPDATE `applicants` SET `activated`='true' WHERE `private_key`='$pk';";
+    $query = mysqli_query(MySqlLeaf::getCon(), $sql);
+
+    if ($query === true){
+        FlashCard::setFlashCard("pkSuccess");
+        echo "Successfull registered";
+    }else{
+        FlashCard::setFlashCard("pkError");
+        echo "An error occurs, please try again later.";
+    }
 }else{
-    echo "Error occurs. Try again later faggot";
+    FlashCard::setFlashCard("pkWrong");
 }
+
+header("location: ../index.php");
 exit;
