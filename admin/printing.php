1b7e5f28c8d1a1c1505446e723dddf47cad297aa
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-09 18:38:41 +0800

    Added Dynamic Printing Form

--- admin/printing.php
+++ admin/printing.php
@@ -1,23 +1,51 @@
+<?php
+
+include '../class/MySqlLeaf.php';
+
+@ $privateKey = $_GET["id"];
+
+if (isset($privateKey)){    
+
+    // SQL Query: Get the data in the private key
+    $sql = "SELECT * FROM `applicants` WHERE `id`='$privateKey' AND `activated`='true'LIMIT 1";
+    
+    // Prepare Query
+    $query = mysqli_query(MySqlLeaf::getCon(), $sql);
+
+    // Count Rows
+    $numRow = mysqli_num_rows($query);
+
+    // Put the MYSQL Result in the user Info
+    if ($numRow > 0){
+        $userInfo = mysqli_fetch_array($query);
+        $renew_request = true;
+    }else{
+        $renew_request = false;
+    }
+    
+}else{
+    $userInfo = array();
+    $renew_request = false;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <script>
+    var tD = new Date();
+    var datestr = tD.getDate() + "/" + (tD.getMonth()+ 1) + "/" + tD.getFullYear();
+    function popDate(eN) {
+    document.forms[0].elements[eN].value = datestr;
+    }
+    </script>
 </head>	 
-<script>
-var tD = new Date();
-var datestr = tD.getDate() + "/" + (tD.getMonth()+ 1) + "/" + tD.getFullYear();
-function popDate(eN) {
-document.forms[0].elements[eN].value = datestr;
-}
-</script>
-
 <body>
 
 <form>
         <!-- ini ang puon kng sa iframe -->	
         <section id="main" class="column">
-
-
-
 
 
         <img src="..\assets\img\pnp.png" style="width:100px;height:120px;border:3;left:310px;top:15px;position: absolute;">
@@ -42,7 +70,7 @@ document.forms[0].elements[eN].value = datestr;
         <h1 style="margin-left:288px;"> <U>POLICE CLEARANCE </U> </h1>
         </center>
                                         
-        <div class="boxed" style = "border:1px solid black; height:200px; width:200px;position:absolute;left:56px;">
+        <div class="boxed" style = "border:1px solid black; height:200px; width:200px;position:absolute;left:56px; ">
         <center>
         Right Thumbmark
         </center>
@@ -55,17 +83,17 @@ document.forms[0].elements[eN].value = datestr;
         <font size= "3"> TO WHOM IT MAY CONCERN
     </p> 
     <p style="text-indent:50px;"> This is to certify that 
-        <input type="text" style="border: none; border-bottom: 1px solid black; width: 410px; "/>
+        <input type="text" value="<?php echo @$userInfo['lname'] .", ". @$userInfo['fname'] .' ' .@$userInfo['suffix'] .' ' .@$userInfo['mname'] ; ?>" style="border: none; border-bottom: 1px solid black; width: 410px; "/>
         age 
-        <input type="number" style="border: none; border-bottom: 1px solid black; width: 80px;"/>
+        <input type="number" value="<?php echo @$userInfo['age']; ?>" style="border: none; border-bottom: 1px solid black; width: 80px;"/>
     </p>
-
+ 
     <p>
         ______________,  born on 
-        <input type="text" style="border: none; border-bottom: 1px solid black; width: 395px; "/> 
+        <input type="text" value="<?php echo @$userInfo['bdate']; ?>"  style="border: none; border-bottom: 1px solid black; width: 395px; "/> 
         , presently residing at 
     </p>     
-        <input type="text" style="border: none; border-bottom: 1px solid black; width: 710px; "/>
+        <input type="text" value="<?php echo @$userInfo['complete_address']; ?>" style="border: none; border-bottom: 1px solid black; width: 710px; "/>
     <p>and &ensp;&ensp;whose &ensp;&ensp;picture, &ensp;&ensp;right thumb mark, &ensp;&ensp;and &ensp;&ensp;signature appear &ensp;&ensp;hereon&ensp;&ensp; has&ensp;&ensp;  undergone 
     </p>
     <p>
@@ -73,7 +101,7 @@ document.forms[0].elements[eN].value = datestr;
     </p>
     <p style="margin-left:50px;">
         FINDINGS:<input type="text" style="border: none; border-bottom: 1px solid black; width: 577px; "  /><br>
-        PURPOSE:<input type="text" style="border: none; border-bottom: 1px solid black; width: 580px; " />
+        PURPOSE:<input type="text" value="<?php echo @$userInfo['purpose']; ?>" style="border: none; border-bottom: 1px solid black; width: 580px; " />
     </p>
 
         <b style="position: absolute; top: 570px; left: 400px;"> RECORD CHECKED BY</b>
@@ -131,12 +159,12 @@ document.forms[0].elements[eN].value = datestr;
     </p>
  
     <p style="text-indent:43px">NAME:
-    <input type="text" style="border: none; border-bottom: 1px solid black; width: 924px; "/>
+    <input type="text" value="<?php echo @$userInfo['lname'] .", ". @$userInfo['fname'] .' ' .@$userInfo['suffix'] .' ' .@$userInfo['mname'] ; ?>"  style="border: none; border-bottom: 1px solid black; width: 924px; "/>
             
     </p>
 
     <p style="text-indent:43px">ADDRESS:
-    <input type="text" style="border: none; border-bottom: 1px solid black; width: 898px; "/>
+    <input type="text"  value="<?php echo @$userInfo['complete_address']; ?>" style="border: none; border-bottom: 1px solid black; width: 898px; "/>
     <p>
     <p style="text-indent:43px">SEX:
     <input type="checkbox" name="sex" value="male">M
@@ -149,23 +177,23 @@ document.forms[0].elements[eN].value = datestr;
              <input type="checkbox" name="civilstatus" >SEPARATED
             </left>   
     </p>
-    <p style="text-indent:43px">DATE OF BIRTH:<input type="date" style="border: none; border-bottom: 1px solid black; width: 125px; ">
-        PLACE OF BIRTH:<input type="text" style="border: none; border-bottom: 1px solid black; width: 284px; "/>
-        RELIGION:<input type="text" style="border: none; border-bottom: 1px solid black; width: 230px; "/>
+    <p style="text-indent:43px" >DATE OF BIRTH:<input type="date" value="<?php echo @$userInfo['bdate']; ?>" style="border: none; border-bottom: 1px solid black; width: 125px; ">
+        PLACE OF BIRTH:<input type="text"  value="<?php echo @$userInfo['birth_place']; ?>" style="border: none; border-bottom: 1px solid black; width: 284px; "/>
+        RELIGION:<input type="text"  value="<?php echo @$userInfo['religion']; ?>" style="border: none; border-bottom: 1px solid black; width: 230px; "/>
     </p>
-    <p style="text-indent:43px">NATIONALITY: <input type="text" style="border: none; border-bottom: 1px solid black; width: 338px; "/>
-        HEIGHT:<input type="text" style="border: none; border-bottom: 1px solid black; width: 181px; "/>
-        WEIGHT:<input type="text" style="border: none; border-bottom: 1px solid black; width: 205px; "/>
+    <p style="text-indent:43px">NATIONALITY: <input type="text"  style="border: none; border-bottom: 1px solid black; width: 338px; "/>
+        HEIGHT:<input type="text"  value="<?php echo @$userInfo['height']; ?>" style="border: none; border-bottom: 1px solid black; width: 181px; "/>
+        WEIGHT:<input type="text"  value="<?php echo @$userInfo['weight']; ?>" style="border: none; border-bottom: 1px solid black; width: 205px; "/>
         
     </p>
-    <p style="text-indent:43px">COMM. TAX CERT. NO:<input type="text" style="border: none; border-bottom: 1px solid black; width: 285px; "/>
+    <p style="text-indent:43px">COMM. TAX CERT. NO:<input type="text"  value="<?php echo @$userInfo['cedula_no']; ?>" style="border: none; border-bottom: 1px solid black; width: 285px; "/>
         ISSUED AT:<input type="text" style="border: none; border-bottom: 1px solid black; width: 162px; "/>
         ISSUED ON:<input type="text" style="border: none; border-bottom: 1px solid black; width: 181px; "/>
         
     </p>
 
     <p style="text-indent:43px">PURPOSE IN SECURING POLICE CLEARANCE
-    <input type="text" style="border: none; border-bottom: 1px solid black; width: 637px; "/>
+    <input type="text" value="<?php echo @$userInfo['purpose']; ?>" style="border: none; border-bottom: 1px solid black; width: 637px; "/>
     </p>
 
 </div>
